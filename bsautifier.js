b33550686abd4eb7326fc2433cbca4f7e369b190
Sergeij Belevskij <bbelevskij@gmail.com>
2016-02-21 12:05:50 +0500

    modify BSAutifier functions visibilities

--- bsautifier.js
+++ bsautifier.js
@@ -9,20 +9,61 @@ function BSAutifier (options) {
   this.tabSize = options.tabSize || 2,
   this.tabChar = options.tabChar || ' ',
   this.line_end_char = options.line_end_char || '\n'
-}
 
-Array.prototype.min = function () {
-  return Math.min.apply(Math, this);
-}
+  // private functions
 
-Array.prototype.removeIf = function(callback) {
-  var i = this.length;
-  while (i--) {
-    if (callback(this[i], i)) {
-      this.splice(i, 1);
-    }
+  self.first = function(v) {
+    return(v ? v[0] : null);
   }
-};
+
+  Array.prototype.min = function () {
+    return Math.min.apply(Math, this);
+  }
+
+  Array.prototype.removeIf = function(callback) {
+    var i = this.length;
+    while (i--) {
+      if (callback(this[i], i)) {
+        this.splice(i, 1);
+      }
+    }
+  };
+
+  this.wich_tag_is_next = function(text, tags) {
+    var closest_opening_tag = first(text.match(tags.opening));
+    var o_position = text.search(closest_opening_tag);       //opening
+    var closest_closing_tag = first(text.match(tags.closing));
+    var c_position = text.search(closest_closing_tag);       //closing
+    var closest_self_closing_tag = first(text.match(tags.self_closing));
+    var sc_position = text.search(closest_self_closing_tag); //self closing
+    var positions = [o_position, c_position, sc_position];
+    positions.removeIf(function(item) {
+      return(item < 0);
+    });
+    closest_position = positions.min();
+    var closest_tag_hash = {}
+    switch(closest_position) {
+      case o_position:
+        closest_tag_hash.position = o_position;
+        closest_tag_hash.tag_content = closest_opening_tag;
+        closest_tag_hash.tag_type = 'o';
+        break;
+      case c_position:
+        closest_tag_hash.position = c_position;
+        closest_tag_hash.tag_content = closest_closing_tag;
+        closest_tag_hash.tag_type = 'c';
+        break;
+      case sc_position:
+        closest_tag_hash.position = sc_position;
+        closest_tag_hash.tag_content = closest_self_closing_tag;
+        closest_tag_hash.tag_type = 'sc';
+        break;
+      default:
+        // text ended
+    }
+    return(closest_tag_hash);
+  }
+}
 
 BSAutifier.prototype.beautify = function(text) {
   var indentTag = this.tabChar.repeat(this.tabSize);
@@ -30,7 +71,7 @@ BSAutifier.prototype.beautify = function(text) {
   var beautified_text = '';
   var next_position_hash;
   while (text.length > 0) {
-    next_position_hash = wich_tag_is_next(text, this.tags);
+    next_position_hash = this.wich_tag_is_next(text, this.tags);
     switch (next_position_hash.tag_type) {
       case null:
         text = '';
@@ -67,42 +108,3 @@ BSAutifier.prototype.beautify = function(text) {
   }
   return beautified_text;
 };
-
-first = function(v) {
-  return(v ? v[0] : null);
-}
-
-wich_tag_is_next = function(text, tags) {
-  var closest_opening_tag = first(text.match(tags.opening));
-  var o_position = text.search(closest_opening_tag);       //opening
-  var closest_closing_tag = first(text.match(tags.closing));
-  var c_position = text.search(closest_closing_tag);       //closing
-  var closest_self_closing_tag = first(text.match(tags.self_closing));
-  var sc_position = text.search(closest_self_closing_tag); //self closing
-  var positions = [o_position, c_position, sc_position];
-  positions.removeIf(function(item) {
-    return(item < 0);
-  });
-  closest_position = positions.min();
-  var closest_tag_hash = {}
-  switch(closest_position) {
-    case o_position:
-      closest_tag_hash.position = o_position;
-      closest_tag_hash.tag_content = closest_opening_tag;
-      closest_tag_hash.tag_type = 'o';
-      break;
-    case c_position:
-      closest_tag_hash.position = c_position;
-      closest_tag_hash.tag_content = closest_closing_tag;
-      closest_tag_hash.tag_type = 'c';
-      break;
-    case sc_position:
-      closest_tag_hash.position = sc_position;
-      closest_tag_hash.tag_content = closest_self_closing_tag;
-      closest_tag_hash.tag_type = 'sc';
-      break;
-    default:
-      // text ended
-  }
-  return(closest_tag_hash);
-}
